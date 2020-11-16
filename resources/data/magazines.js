945c3dca2e153c903b8fb98348f743ac585c3a73
Anand Vaidya <anand1711@gmail.com>
2020-11-16 16:07:39 +0550

    updated changes - 2020-11-16 15:47:39

--- resources/data/magazines.js
+++ resources/data/magazines.js
@@ -3,3 +3,10 @@ magazines = [
   "https://flipboard.com/@dfletcher/india-tech-b2meqpd6z.rss",
   "https://flipboard.com/@thehindu/sportstarlive-rj3ttinvz.rss"
 ]
+
+// magazines = [
+//   "https://storage.googleapis.com/fullstack-assessment/coronavirus.rss",
+//   "https://storage.googleapis.com/fullstack-assessment/india-tech.rss",
+//   "https://storage.googleapis.com/fullstack-assessment/sports-star.rss"
+// ]
+
